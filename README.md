5380aafbd08cab55d13b1f8a5bf6c3d4c153f12b
Kuba Szatan <82782621+ProgramistaAbuk@users.noreply.github.com>
2025-11-05 22:27:05 -0500

    V1.0
    Abrix base module functioning

--- README.md
+++ README.md
@@ -1,3 +1,149 @@
-# abrix
+<h1 align=center>abrix</h1>
+
+<div align=center>
+![GitHub last commit](https://img.shields.io/github/last-commit/ProgramistaAbuk/abrix?style=for-the-badge&labelColor=101418&color=9ccbfb)
+![GitHub repo size](https://img.shields.io/github/repo-size/ProgramistaAbuk/abrix?style=for-the-badge&labelColor=101418&color=d3bfe6)
+</div>
+
+Mini script I made in 2 days used mainly take .war files from projects, compile them using mvn, and put them into local tomcat to test.
+Program is also capable of downloading and preparing a new tomcat folder. Program hashes folder to check weather changes occured.
+
+### TODOOOOOOOOOOOOOO
+- Line `474` | add actual download of JAVA jdk (scan for all versions, let user choose, then set it up)
+
+- Install + uninstall script
+ 
+
+### parameters
+`-h --help` View all params
+
+`-a --al` Skip change detection, compile all wars
+
+`-c --config [OPTION]`
+
+- `gen` generate config file
+- `edit` right now nothing but in future edit config from program not manually
+
+`-sv --savechanges` Saves current changes to `abrix.dat.hash` file
+
+`-t --tomcat [TOMCAT]` Enter directory for the tomcat you want abrix to put .war files in (only affects one run of program - to change default tomcat directory, see editing config)
+
+`-ct --createtomcat [DIRECTORY]` Auto downloads, unzips, and sets up tomcat in specified directory, also changes config file to point to newly downloaded tomcat (to set what tomcat version you want downloaded see editing config)
+
+`-jm --javamanual [DIRECTORY]` Forces program to use a specific directory of JAVA (keep in mind program doesn't check if the directory actually has JAVA in it)
 
 
+### editing config
+Config file is structured using
+```commandline
+[TAGNAME] {
+
+}
+```
+tags.
+Possible tag names are:
+- `PROJECTS` For setting project directories
+- `TOMCAT` To set default tomcat directory
+- `PRFNA` (Project File Names), to set the .war file names that maven sets once it compiles based on `pom.xml`
+- `PRFNNA` (Project File New Names), what you want abrix to set your .war files in new tomcat
+- `TOMCATV` for parameter `--createtomcat`, param in this tag tells program what tomcat versison to download
+
+#### Inside tags
+Inside tags all you can do is set variables, variable names aren't checked in the program.
+> [!NOTE]
+> You can set variables to whatever name you may want, EXCLUDING
+> ```commandline
+> [TOMCAT] {
+> set t = tomcatdir
+> }
+> ```
+> This is edited when you create tomcat (using --createtomcat), as such variable name 't' cannot be changed
+ 
+- The `PROJECTS` tag
+
+In the projects tag, by default there are variable names pr1, pr2 (project 1, project 2).
+Inside of this tag you set the project directories you want abrix to check for when running.
+When setting directories always use `/` between folder names and always point to your project directory,
+not  directories within your project. You may set as many projects as you want.
+
+- The `TOMCAT` tag
+
+In here you set your tomcat directory (top folder of tomcat directory), e.g
+```commandline
+[TOMCAT] {
+set t = YourTomcatDirectoryHere
+```
+Keep in mind that for this tag you cannot change the variable name 't'.
+
+- The `PRFNA` tag
+
+In this tag you set the name of your compiled .wars, when using mvn clean install, it generates a war with a specific name, input that name here.
+Keep in mind to put in file extension  e.g `CompileFileName.war`, you can change the compiled file name in your projects pom.xml
+
+- The `PRFNNA` tag
+
+Here you set the new war file names, when abrix copies the compiled mvn files to your tomcat, this is the name it will set them to.
+So if you want tomcat to deploy at `/projectname`, your .war must be `projectname.war`  and that is what you set in this tag.
+
+- The `TOMCATV` tag
+
+The only purpose of this tag is when using param `--createtomcat`, what you input here will be the version of tomcat abrix downloads and sets up.
+Keep in mind to use  the full version, e.g '10.1.3' not '10'
+
+
+- General info on tags
+
+All tags have the same syntax after [TAGNAME] {}, you put in variables as such:
+```commandline
+set varname = varvalue
+```
+One thing to keep in mind is whitespaces, program finds you var based on whitespaces, and thus
+you must (should) only have 'set' 'space' 'varname' '=' 'varvalue' | keeping in mind none of the names can have spaces within them,
+e.g varname and varvalue CANNOT have spaces between them.
+
+Example config looks as such:
+```commandline
+#Main Abrix Config File
+[PROJECTS] {
+set pr1 = C:/__p__/--py--/abrix/test/proxyservice
+set pr2 = C:/__p__/--py--/abrix/test/rating
+}
+[TOMCAT] {
+set t = C:/debug/test67/tomcat-11
+}
+[PRFNA] {
+set pr1 = proxyservice-2.1.war
+set pr2 = rating-0.0.2-SNAPSHOT.war
+}
+[PRFNNA] {
+set pr1 = proxyservice.war
+set pr2 = ratingservice.war
+}
+[TOMCATV] {
+set v = 11.0.13
+}
+```
+
+### program files
+Abrix uses only 3 files for data, logs, config and 4 files for the program itself
+
+- Inside the `logs` folder is a `log.out` file where all logs of abrix are saved.
+
+
+- `conf folder`
+
+In the conf folder there are two files:
+- `abrix.conf`
+Where you put your configuration (this is where you would put what was said above)
+- `abrix.dat.hash`
+Program data file - DO NOT EDIT. This is where abrix stores hashes to later compare and see if project has any new changes.
+
+
+### more
+- Program assumes there is maven installed and in PATH, and that maven/bin has a mvn.cmd file.
+To download maven click [`here`](https://maven.apache.org/download.cgi)
+- Program will automatically detect if TOMCAT fails because of improper setting of JAVA_HOME and will ask user to choose JAVA version (program scans
+for all JDK's on system), once user chooses, abrix will either (depending on user input), put in the env var in the startup file for tomcat,
+or just do a single startup with that JAVA_HOME env
+- If you get to page where program asks if install JAVA manually, that feature doesn't exist yet,
+if someone would like to add it go to `main.py` line `474`
--- main.py
+++ main.py
@@ -8,8 +8,65 @@ import shutil
 import re
 import hashlib
 import os
+import subprocess
+import platform
+import urllib.request
+import zipfile
+import webbrowser
 import inter
 import pV
+
+tomcatV = ""
+globalTomcatDir = ""
+
+
+
+def find_all_java_windows():
+    java_dirs = [
+        r"C:\Program Files\Java",
+        r"C:\Program Files (x86)\Java",
+        r"C:\Program Files\Common Files\Oracle\Java"
+    ]
+
+    found = []
+
+    for base in java_dirs:
+        if not os.path.exists(base):
+            continue
+        for entry in os.listdir(base):
+            java_path = os.path.join(base, entry, "bin", "java.exe")
+            if os.path.isfile(java_path):
+                try:
+                    result = subprocess.run(
+                        [java_path, "-version"], capture_output=True, text=True
+                    )
+                    version_info = result.stderr.strip() or result.stdout.strip()
+                    found.append((java_path, version_info.splitlines()[0]))
+                except Exception:
+                    continue
+    return found
+
+
+
+def find_all_java_unix():
+    java_dirs = ["/usr/lib/jvm", "/usr/java", "/Library/Java/JavaVirtualMachines"]
+    found = []
+
+    for base in java_dirs:
+        if not os.path.exists(base):
+            continue
+        for entry in os.listdir(base):
+            java_path = os.path.join(base, entry, "bin", "java")
+            if os.path.isfile(java_path) and os.access(java_path, os.X_OK):
+                try:
+                    result = subprocess.run([java_path, "-version"], capture_output=True, text=True)
+                    version_info = result.stderr.strip() or result.stdout.strip()
+                    found.append((java_path, version_info.splitlines()[0]))
+                except Exception:
+                    continue
+    return found
+
+
 
 class SD:
     def __init__(self, hash):
@@ -36,8 +93,6 @@ class SD:
                 if l[0].split() == ['False']:
                     with open("conf/abrix.dat.hash", 'a') as k:
                         k.write(f'{self.hash}\n')
-                else:
-                    print('read mode')
         else:
             with open("conf/abrix.dat.hash", "a") as f:
                 f.write('False\n')
@@ -124,7 +179,28 @@ class Config:
     def gen(self):
         os.makedirs("conf", exist_ok=True)
         with open("conf/abrix.conf", "w") as f:
-            f.write(f'#Main Abrix Config File')
+            config = """\
+#Main Abrix Config File
+[PROJECTS] {
+set pr1 = C:/__p__/--py--/abrix/test/proxyservice
+set pr2 = C:/__p__/--py--/abrix/test/rating
+}
+[TOMCAT] {
+set t = C:/debug/test67/tomcat-11
+}
+[PRFNA] {
+set pr1 = proxyservice-2.1.war
+set pr2 = rating-0.0.2-SNAPSHOT.war
+}
+[PRFNNA] {
+set pr1 = proxyservice.war
+set pr2 = ratingservice.war
+}
+[TOMCATV] {
+set v = 11.0.13
+}
+"""
+            f.write(config)
 
 
 class Log:
@@ -157,6 +233,17 @@ class Log:
 # [2025-11-02 | 21:06:05.434] ERROR - complete
 
 
+class Mvn:
+    def __init__(self, path):
+        self.path = path
+    def runMvn(self):
+        resuslt = subprocess.run(["mvn.cmd", "clean", "install"],
+        cwd=self.path,     stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True
+        )
+        Log(f"Maven result {resuslt.stdout}", "N").write()
+
 
 def clean():
     Log("Clean up", 'N').write()
@@ -173,7 +260,238 @@ def fold():
         sys.exit("Directory 'tmp' already exists, please delete it and try again")
     os.makedirs('logs', exist_ok=True)
 
+
+def update_tomcat_path(new_path):
+    # Convert backslashes to forward slashes (fixes \t being interpreted as tab)
+    new_path = new_path.replace("\\", "/")
+
+    with open("conf/abrix.conf", "r") as f:
+        text = f.read()
+
+    # Replace only the value after 'set t =' in the [TOMCAT] section
+    text = re.sub(
+        r"(set t\s*=\s*)[^\n\r]+",
+        r"\1" + new_path,
+        text
+    )
+
+    with open("conf/abrix.conf", "w") as f:
+        f.write(text)
+
+def tomcat(path):
+    global tomcatV
+    tCut = tomcatV[0:2]
+    if tCut[1] == '.':
+        tCut = tomcatV[0:1]
+    os_name = platform.system()
+    url = ""
+    if os_name == "Windows":
+        url = f"https://dlcdn.apache.org/tomcat/tomcat-{tCut}/v{tomcatV}/bin/apache-tomcat-{tomcatV}-windows-x64.zip"
+    elif os_name == "Linux":
+        url = f"https://dlcdn.apache.org/tomcat/tomcat-{tCut}/v{tomcatV}/bin/apache-tomcat-{tomcatV}.zip"
+
+    os.makedirs(path, exist_ok=True)
+    tomcat_folder = os.path.join(path, "tomcat-11")
+    os.makedirs(tomcat_folder, exist_ok=True)
+    Log("NEW TOMCAT FOLDER IS" + tomcat_folder, "N").write()
+    # print("TOMCAT FOLDER", tomcat_folder)
+    # Download zip
+    zip_path = os.path.join(path, "tomcat-11.zip")
+    urllib.request.urlretrieve(url, zip_path)
+
+    # Extract
+    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+        # Get list of all files
+        members = zip_ref.namelist()
+
+        # Find common root directory
+        root = members[0].split('/')[0] if members else ''
+
+        for member in members:
+            # Remove the root directory from path
+            if member.startswith(root + '/'):
+                target_path = member[len(root) + 1:]
+            else:
+                target_path = member
+
+            # Skip if it's just the root directory itself
+            if not target_path:
+                continue
+
+            # Extract to tomcat_folder with modified path
+            source = zip_ref.open(member)
+            target = os.path.join(tomcat_folder, target_path)
+
+            # Create directories if needed
+            if member.endswith('/'):
+                os.makedirs(target, exist_ok=True)
+            else:
+                os.makedirs(os.path.dirname(target), exist_ok=True)
+                with open(target, 'wb') as f:
+                    f.write(source.read())
+
+    Log("Downloaded, unzipped and prepared tomcat", "S").write()
+    update_tomcat_path(tomcat_folder)
+    Log("Config updated to reflect tomcat new path", "S").write()
+    clean()
+    Log("Completed get new tomcat - re run program with new parameters", "N").write()
+    sys.exit("Completed get new tomcat - re run program with new parameters")
+
+def javaInstall():
+    try:
+        result = subprocess.run(
+            ["java", "-version"],
+            capture_output=True, text=True, check=True
+        )
+        # Java prints version to stderr usually
+        output = result.stderr.strip() or result.stdout.strip()
+        print("Java found:\n", output)
+        return True
+    except (subprocess.CalledProcessError, FileNotFoundError):
+        print("Java not found on system.")
+        return False
+
+
+def stopTomcat(path):
+    os_name = platform.system()
+
+    if os_name == "Windows":
+        result = subprocess.run(
+            ["cmd", "/c", "shutdown.bat"],
+            cwd=f"{path}/bin",
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True
+        )
+    elif os_name == "Linux" or os_name == "Darwin":
+        result = subprocess.run(
+            ["sh", "shutdown.sh"],
+            cwd=f"{path}/bin",
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True
+        )
+
+    else:
+        raise Exception(f"Unsupported OS: {os_name}")
+
+    Log(f"Stopped tomcat - Result is: {result.stdout}", "S").write()
+
+
+def addjhome(path, java_home):
+    is_windows = platform.system() == 'Windows'
+
+    if is_windows:
+        script_file = f'{path}/bin/startup.bat'
+        java_line = f'set JAVA_HOME={java_home}\n'
+    else:
+        script_file = f'{path}/bin/startup.sh'
+        java_line = f'export JAVA_HOME={java_home}\n'
+
+    with open(script_file, 'r') as f:
+        lines = f.readlines()
+
+    lines.insert(1, java_line)
+
+
+    with open(script_file, 'w') as f:
+        f.writelines(lines)
+
+
+def startTomcat(path):
+    os_name = platform.system()
+
+    if os_name == "Windows":
+        result = subprocess.run(
+            ["cmd", "/c", "startup.bat"],
+            cwd=os.path.join(path, "bin"),
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True
+        )
+    elif os_name == "Linux" or os_name == "Darwin":
+        result = subprocess.run(
+            ["sh", "startup.sh"],
+            cwd=os.path.join(path, "bin"),
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True
+        )
+
+    else:
+        raise Exception(f"Unsupported OS: {os_name}")
+
+    Log(f"Ran tomcat - Result is: {result.stdout}", "S").write()
+    er = "Result is: Neither the JAVA_HOME nor the JRE_HOME environment variable is defined"
+    if "JAVA_HOME" in result.stdout and "is defined" in result.stdout:
+        Log("Java not properly configured", "E").write()
+        a = input(f"Set up Java in TOMCAT {path} startup file? (Y/n): ")
+        if a == "Y" or a == "y":
+            jI = javaInstall()
+            if jI:
+                Log("Abrix found an installed version of java on this machine", "S").write()
+                print(f"Please select one of the following JAVA versions to use for TOMCAT {path}")
+                os_name = platform.system()
+
+                if os_name == "Windows":
+                    f = find_all_java_windows()
+                else:
+                    f  = find_all_java_unix()
+
+
+                for c, i in enumerate(f):
+                    print(f"{c}) JAVA in: {i[0]} ||| version: {i[1]}")
+
+
+                a = int(input("What java to use (enter single number e.g '1' or '2') ?: "))
+                grandparent = os.path.dirname(os.path.dirname(f[a][0]))
+                Log(f"User selected to use JAVA {f[a]}", "S").write()
+                ct = input(f"Add permanent JAVA_HOME to your {path} tomcat startup file? (Y/n): ")
+                if ct == "Y" or ct == "y":
+                    addjhome(path, grandparent)
+                    startTomcat(path)
+                else:
+                    env = os.environ.copy()
+                    env['JAVA_HOME'] = grandparent
+                    env['CATALINA_HOME'] = path
+                    env['CATALINA_BASE'] = path  # Often the same as CATALINA_HOME
+
+                    if os_name == 'Windows':
+                        script = f'{path}/bin/startup.bat'
+                    else:
+                        script = f'{path}/bin/startup.sh'
+
+                    subprocess.run([script], env=env)
+
+
+            else:
+                Log("No JAVA installation found on system", "E").write()
+                a = input("Find and install JAVA on system? (Y/n): ")
+                if a == "Y" or a == "y":
+                    print("You thought this would actually install JAVA - the least you can do is install it yourself")
+                    print("Yeah no I was just too lazy to add something that finds java jdk versions, asks user what  one to do and then actually install and extract - too much work")
+                    print("If you see this and want to make the function yourself feel free to submit pull request")
+                    print("Go to around line 474 - that's where this message is")
+                    Log("Denied auto install - must install manually before running program", "E").write()
+                    url = "https://www.oracle.com/java/technologies/downloads/"
+                    webbrowser.open(url)
+                    clean()
+                    sys.exit("Please install JAVA properly")
+                else:
+                    Log("Denied auto install - must install manually before running program", "E").write()
+                    url = "https://www.oracle.com/java/technologies/downloads/"
+                    webbrowser.open(url)
+                    clean()
+                    sys.exit("Please install JAVA properly")
+        else:
+            Log("User cancelled and tomcat cannot run as Java is improperly configured", "E").write()
+            clean()
+            sys.exit(f"Please configure JAVA_HOME or JRE_HOME and confirm tomcat can run at dir {path} before telling this program to run tomcat")
+
+
+
 def main():
+
     parser = argparse.ArgumentParser(description='Description of your program')
     parser.add_argument('-a', '--all', action='store_true', help='Overwrite check changes, compile all wars')
     parser.add_argument(
@@ -183,12 +501,31 @@ def main():
         help='Edit program config or "gen" to generate a new one'
     )
     parser.add_argument('-sc', '--savechanges', action='store_true', help='Hash and save all project changes')
+    parser.add_argument(
+        '-t', '--tomcat',
+        nargs='?',
+        help='Set tomcat directory to one other than in config'
+    )
+    parser.add_argument(
+        '-ct', '--createtomcat',
+        nargs='?',
+        help='Download and prepare tomcat for local use'
+    )
+    parser.add_argument(
+        '-jm', '--javamanual',
+        nargs='?',
+        help='Force specific folder to be JAVA for tomcat'
+    )
     # parser.add_argument('-v', '--verbose', action='store_true', help='Enable verbose mode')
     # parser.add_argument('filename', help='Positional argument (required)')
     args = parser.parse_args()
     Log(f'User parsed --a {args.all} arguments', 'N').write()
     Log(f'User parsed --c {args.config} arguments', 'N').write()
     Log(f'User parsed --sc {args.savechanges} arguments', 'N').write()
+    Log(f'User parsed --t {args.tomcat} arguments', 'N').write()
+    Log(f'User parsed -ct {args.createtomcat} arguments', 'N').write()
+    Log(f'User parsed -jm {args.javamanual} arguments', 'N').write()
+
     if args.config == "gen":
         Config().gen()
         Log("Generated config, restart program - clean omitted, tmp never created", "S").write()
@@ -199,17 +536,127 @@ def main():
         if os.path.exists("conf/abrix.dat.hash"):
             os.remove("conf/abrix.dat.hash")
         for i in pV.vars:
-            for a in pV.vars[i]:
-                Log(f"Folder check is {a[1]}", "N").write()
-                hash = CheckChange(a[1]).hash_folder()
-                Log(f"Hashed to {hash}", 'S').write()
-                SD(hash).cF()
+            if i == 'PROJECTS':
+                for a in pV.vars[i]:
+                    Log(f"Folder check is {a[1]}", "N").write()
+                    hash = CheckChange(a[1]).hash_folder()
+                    Log(f"Hashed to {hash}", 'S').write()
+                    SD(hash).cF()
         SD("").lock()
+        Log("Changes saved, rerun program", "S").write()
+        clean()
+        sys.exit("Changes saved, rerun program")
+    with open("conf/abrix.dat.hash", "r") as f:
+        c = f.readlines()
 
+    oldFileNames = {}
+    newFileNames = {}
+    for i in pV.vars:
+        if i == "PRFNA":
+            Log("Getting old file names for each project", "N").write()
+            for r,a in enumerate(pV.vars[i]):
+                Log("Old file name is " + a[1], "N").write()
+                oldFileNames[r] = a[1]
+        elif i == "PRFNNA":
+            Log("Getting new file names for each project", "N").write()
+            for r,a in enumerate(pV.vars[i]):
+                Log("New file name is " + a[1], "N").write()
+                newFileNames[r] = a[1]
+        elif i == "TOMCATV":
+            for a in pV.vars[i]:
+                global tomcatV
+                tomcatV = a[1]
+
+    if args.createtomcat is not None:
+        tomcat(args.createtomcat)
+
+    Log(f"After retrival old file names are {oldFileNames}", "N").write()
+    Log(f"After retrival new file names are {newFileNames}", "N").write()
+    for i in pV.vars:
+        if i == 'PROJECTS':
+            for r, a in enumerate(pV.vars[i]):
+                hash = CheckChange(a[1]).hash_folder()
+                Log(f"Hash check is {hash} {c[r+1]}", "N").write()
+                if (hash != c[r+1].strip()) or args.all:
+                    try:
+                        Mvn(a[1]).runMvn()
+                        Log(f"Starting war copy from default to tmp folder for {a[1]}", "N").write()
+                        shutil.copy(f"{a[1]}/target/{oldFileNames[r]}", f'tmp/{newFileNames[r]}')
+                    except Exception as e:
+                        Log(f"Encountered exception {e}", 'E').write()
+                        sys.exit(f"Encountered exception {e} | MANUAL INTERVENTION REQUIRED")
+                else:
+                    Log(f"Maven compile skipped as no changes detected and -a param not used", "S").write()
+                    clean()
+                    sys.exit("No changes detected and -a param not used")
+        elif i == 'TOMCAT':
+            global globalTomcatDir
+            if args.tomcat is not None:
+                globalTomcatDir = args.tomcat
+                Log(f"Manual override of tomcat dir detected - changed to {globalTomcatDir} ", "S").write()
+            else:
+                Log(f"Tomcat pV.vars[i] is {pV.vars[i]}", "N").write()
+                globalTomcatDir = pV.vars[i][0][1]
+
+    if args.javamanual is not None:
+        globalTomcatDir = args.javamanual
+    Log("Asking user to confirm send to local tomcat set dir", "N").write()
+    Log(f"The following values go in order, [1] is oldFileNames [2] is the newFileNames (what will be in tomcat) [3] is tomcat dir [4] is files that will be put into tomcat", "N").write()
+    Log(oldFileNames, "N").write()
+    Log(newFileNames, "N").write()
+    Log(globalTomcatDir, "N").write()
+    files = [
+        entry.name
+        for entry in os.scandir("tmp")
+        if entry.is_file() and entry.name.endswith(".war")
+    ]
+
+    Log(files, "N").write()
+    a = input("Confirm send war files to local tomcat? (Y/n): ")
+    if a == "Y" or a == "y":
+        Log("User confirmed, proceeding", "S").write()
+        Log("Stopping tomcat", "N").write()
+        stopTomcat(globalTomcatDir)
+        for i in files:
+            try:
+                Log(f"Copying file {i}", "N").write()
+                shutil.move(f"tmp/{i}", f"{globalTomcatDir}/webapps/{i}")
+            except Exception as e:
+                Log(f"Encountered exception while copying file {i}- canceling {e}", 'E').write()
+                clean()
+                sys.exit(f"Encountered exception while copying file {i}- canceling {e}")
+        has_war = any(
+            entry.is_file() and entry.name.lower().endswith(".war")
+            for entry in os.scandir("tmp")
+        )
+        files2 = [
+            entry.name
+            for entry in os.scandir("tmp")
+            if entry.is_file() and entry.name.endswith(".war")
+        ]
+        if not has_war:
+            Log("ALL WAR WERE COPIED INTO TOMCAT", "S").write()
+            Log("Starting up tomcat", "N").write()
+
+            startTomcat(globalTomcatDir)
+            Log("Starting tomcat tail log", "N").write()
+            print("bye :)")
+        else:
+            Log(f"Not all files copied, stopping all operations - failed to copy {files2}", "E").write()
+            clean()
+            sys.exit(f"Not all files copied, stopping all operations - failed to copy {files2}")
+
+    else:
+        Log("User cancelled, canceling", "E").write()
+        clean()
+        sys.exit("User cancelled, canceling")
     clean()
 
 
 if __name__ == '__main__':
     main()
+
+
+
 
 
